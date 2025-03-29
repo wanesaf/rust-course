3d5265bb7659dbbdf1ca39fc87f6b26f51fdea58
wanesaf <safwanesouhil.dendani@etu.usthb.dz>
2025-03-30 00:10:38 +0100

    the main of the lib

--- oop/src/bin/oopPatern.rs
+++ oop/src/bin/oopPatern.rs
@@ -1,3 +1,10 @@
+use oop::Post ; 
 fn main () {
-    
+    let mut post = Post::new() ; 
+    post.add_text("I ate a salade ");
+    assert_eq!("",post.content());
+    post.request_review(); 
+    assert_eq!("",post.content()); 
+    post.approve() ; 
+    assert_eq!("I ate a salade ",post.content()); 
 }
